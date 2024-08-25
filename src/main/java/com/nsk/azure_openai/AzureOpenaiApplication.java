c906073f664b7516c8c5845ee50c27a98b2137a3
devnsk <devnsk.2024@gmail.com>
2024-08-26 00:30:39 +0530

    initial setup

---
+++ src/main/java/com/nsk/azure_openai/AzureOpenaiApplication.java
@@ -0,0 +1,13 @@
+package com.nsk.azure_openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureOpenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureOpenaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsk/azure_openai/AzureOpenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsk.azure_openai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureOpenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
