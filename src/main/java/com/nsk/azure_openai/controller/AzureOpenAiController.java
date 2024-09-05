155fb219f1b07cd67b6ac0984010ec67cf97772b
devnsk <devnsk.2024@gmail.com>
2024-09-06 02:29:50 +0530

    commentout whole code for now

--- src/main/java/com/nsk/azure_openai/controller/AzureOpenAiController.java
+++ src/main/java/com/nsk/azure_openai/controller/AzureOpenAiController.java
@@ -1,41 +1,41 @@
-package com.nsk.azure_openai.controller;
+//package com.nsk.azure_openai.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.ai.azure.openai.AzureOpenAiChatModel;
+//import org.springframework.ai.chat.model.ChatResponse;
+//import org.springframework.ai.chat.model.Generation;
+//import org.springframework.ai.chat.prompt.Prompt;
+//import org.springframework.ai.chat.prompt.PromptTemplate;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/openai")
+//public class AzureOpenAiController {
+//	private AzureOpenAiChatModel azureOpenAiChatModel;
+//
+//	public AzureOpenAiController(AzureOpenAiChatModel azureOpenAiChatModel) {
+//		super();
+//		this.azureOpenAiChatModel = azureOpenAiChatModel;
+//	}
+//	
+//	@GetMapping
+//	public List<Generation> chat(){
+//		Prompt prompt = new Prompt("What model are you and expain yourself !");
+//		ChatResponse res = azureOpenAiChatModel.call(prompt);
+//		return res.getResults();
+//	}
+//	
+//	@PostMapping("/chat/{msg}")
+//	public List<Generation> trending(@PathVariable String msg){
+//		PromptTemplate promptTemplate = new PromptTemplate(msg);
+//		Prompt prompt = promptTemplate.create();
+//		ChatResponse chatResponse = azureOpenAiChatModel.call(prompt);
+//		return chatResponse.getResults();
+//	}
 
-import java.util.List;
-
-import org.springframework.ai.azure.openai.AzureOpenAiChatModel;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.model.Generation;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/openai")
-public class AzureOpenAiController {
-	private AzureOpenAiChatModel azureOpenAiChatModel;
-
-	public AzureOpenAiController(AzureOpenAiChatModel azureOpenAiChatModel) {
-		super();
-		this.azureOpenAiChatModel = azureOpenAiChatModel;
-	}
-	
-	@GetMapping
-	public List<Generation> chat(){
-		Prompt prompt = new Prompt("What model are you and expain yourself !");
-		ChatResponse res = azureOpenAiChatModel.call(prompt);
-		return res.getResults();
-	}
-	
-	@PostMapping("/chat/{msg}")
-	public List<Generation> trending(@PathVariable String msg){
-		PromptTemplate promptTemplate = new PromptTemplate(msg);
-		Prompt prompt = promptTemplate.create();
-		ChatResponse chatResponse = azureOpenAiChatModel.call(prompt);
-		return chatResponse.getResults();
-	}
-
-}
+//}
